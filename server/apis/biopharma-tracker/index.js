c7a81d02a25b599017c6eea3584f2be858d40b42
Adei Mandaluniz <a.mandaluniz@gmail.com>
2024-11-22 11:04:04 +0100

    chore: code clean up [skip ci]

--- server/apis/biopharma-tracker/index.js
+++ server/apis/biopharma-tracker/index.js
@@ -10,7 +10,7 @@ const stampRouter = require("./routes/notifications")
 const aiRouter = require("./routes/ai")
 
 const app = express();
-app.use(cors())
+app.use(cors());
 app.use(express.json())
 
 app.get("/", (req, res, next) => {

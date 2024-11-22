54f143bb2c7f739ac4b48e2819bc372810690374
Adei Mandaluniz <a.mandaluniz@gmail.com>
2024-11-22 11:44:16 +0100

    fix: get batch details [skip ci]

--- server/apis/biopharma-tracker/routes/items.js
+++ server/apis/biopharma-tracker/routes/items.js
@@ -1,67 +1,16 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
-const stampData = require("../services/stamp");
-const getDb = require("../db");
 const generate = require("../qrCode");
-
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const mongodb = getDb();
-
-const COLLECTION = "batch-items";
+const { getBatchItem, list } = require("../services/items");
 
 router.get("/list", async (req, res, next) => {
     const { batchId } = req.params;
 
-    const { limit = 10, skip = 0, sort = "expirationDate", direction = "ASC" } = req.query;
+    const { limit = 10, skip = 0, sort = "itemNumber", direction = "ASC" } = req.query;
 
-    const sortOp = {
-        [sort]: direction === "ASC" ? 1 : -1
-    }
+    const response = await list(batchId, limit, skip, sort, direction);
 
-    try {
-        await mongodb.connect();
-
-        const [{ total: [total = 0], edges }] = await mongodb
-            .db("biopharma-tracker")
-            .collection(COLLECTION)
-            .aggregate([
-                { 
-                    $match: {
-                        batchId: batchId
-                    }
-                },
-                {
-                    $facet: {
-                      total: [
-                        { $group: { _id: null, count: { $sum: 1 } } },
-                      ],
-                      edges: [
-                        { $sort: sortOp },
-                        { $skip: typeof skip === 'string' ? Number.parseInt(skip) : skip },
-                        { $limit: typeof limit === 'string' ? Number.parseInt(limit) : limit },
-                      ],
-                    },
-                  },
-                  {
-                    $project: {
-                      total: '$total.count',
-                      edges: '$edges',
-                    },
-                }
-            ]).toArray();
-
-            return res.status(200).json({
-                total,
-                pageSize: edges.length,
-                items: edges
-            });
-    } catch (e) {
-        console.log(e);
-    } finally {
-        // Ensures that the client will close when you finish/error
-        if (mongodb) await mongodb.close();
-    }
+    return res.status(200).json(response);
 
 });
 
@@ -71,35 +20,15 @@ router.get("/:itemId", async (req, res, next) => {
 
     console.log('Batch ID', batchId);
 
-    try {
+    const item = await getBatchItem(batchId, itemId);
 
-        console.log('Looking batch up');
-        await mongodb.connect();
-
-        console.log('Connected to DB');
-
-        const batch = await mongodb
-            .db("biopharma-tracker")
-            .collection(COLLECTION)
-            .findOne({ _id: batchId });
-
-        console.log('Batch', batch);
-
-        if (!batch) {
-            return res.status(404).json({
-                error: 'Batch not found'
-            });
-        }
-
-        return res.status(200).json(batch);
-
-        // Create QR Code
-    } catch (e) {
-        console.log(e);
-    } finally {
-        // Ensures that the client will close when you finish/error
-        if (mongodb) await mongodb.close();
+    if (!item) {
+        return res.status(404).json({
+            error: 'Batch item not found'
+        });
     }
+
+    return res.status(200).json(item);
 });
 
 /**
@@ -109,51 +38,26 @@ router.get("/:itemId/qr-code", async (req, res, next) => {
 
     const { batchId, itemId } = req.params;
 
-    try {
-        await mongodb.connect();
+    const item = await getBatchItem(batchId, itemId);
 
-        const item = await mongodb
-            .db("biopharma-tracker")
-            .collection(COLLECTION)
-            .findOne({ 
-                _id: itemId,
-                batchId
-            });
-
-        const batch = await mongodb
-            .db("biopharma-tracker")
-            .collection("batches")
-            .findOne({ 
-                _id: batchId
-            });
-
-        if (!item) {
-            return res.status(404).json({
-                error: 'Item not found'
-            });
-        }
-
-        const qrData = {
-            id: item._id,
-            batchId: item.batchId,
-            itemNumber: item.itemNumber,
-            brand: batch.brand,
-            productId: batch.productId,
-            productType: batch.productType,
-            batchNumber: batch.batchNumber,
-            expirationDate: batch.expirationDate
-        };
-        const qrImage = await generate(qrData)
-        res.send(qrImage);
-
-        // Create QR Code
-    } catch (e) {
-        console.log(e);
-    } finally {
-        // Ensures that the client will close when you finish/error
-        if (mongodb) await mongodb.close();
+    if (!item) {
+        return res.status(404).json({
+            error: 'Item not found'
+        });
     }
 
+    const qrData = {
+        id: item._id,
+        batchId: item.batchId,
+        itemNumber: item.itemNumber,
+        brand: item.batch.brand,
+        productId: item.batch.productId,
+        productType: item.batch.productType,
+        batchNumber: item.batch.batchNumber,
+        expirationDate: item.batch.expirationDate
+    };
+    const qrImage = await generate(qrData)
+    res.send(qrImage);
 });
 
 
--- server/apis/biopharma-tracker/services/items.js
+++ server/apis/biopharma-tracker/services/items.js
@@ -38,6 +38,99 @@ const createItems = async (batchId, numItems) => {
     }
 };
 
+const getBatchItem = async (batchId, itemId) => {
+    try {
+        await mongodb.connect();
+
+        const item = await mongodb
+            .db("biopharma-tracker")
+            .collection(COLLECTION)
+            .findOne({ 
+                _id: itemId,
+                batchId
+            });
+
+        const batch = await mongodb
+            .db("biopharma-tracker")
+            .collection("batches")
+            .findOne({ 
+                _id: batchId
+            });
+
+        if (!item) {
+            return res.status(404).json({
+                error: 'Item not found'
+            });
+        }
+
+        return {
+            ...item,
+            batch
+        }
+
+        // Create QR Code
+    } catch (e) {
+        console.log(e);
+    } finally {
+        // Ensures that the client will close when you finish/error
+        if (mongodb) await mongodb.close();
+    }
+}
+
+const list = async (batchId, limit = 10, skip = 0, sort = "itemNumber", direction = "ASC" ) => {
+
+    const sortOp = {
+        [sort]: direction === "ASC" ? 1 : -1
+    }
+
+    try {
+        await mongodb.connect();
+
+        const [{ total: [total = 0], edges }] = await mongodb
+            .db("biopharma-tracker")
+            .collection(COLLECTION)
+            .aggregate([
+                { 
+                    $match: {
+                        batchId: batchId
+                    }
+                },
+                {
+                    $facet: {
+                      total: [
+                        { $group: { _id: null, count: { $sum: 1 } } },
+                      ],
+                      edges: [
+                        { $sort: sortOp },
+                        { $skip: typeof skip === 'string' ? Number.parseInt(skip) : skip },
+                        { $limit: typeof limit === 'string' ? Number.parseInt(limit) : limit },
+                      ],
+                    },
+                  },
+                  {
+                    $project: {
+                      total: '$total.count',
+                      edges: '$edges',
+                    },
+                }
+            ]).toArray();
+
+            return {
+                total,
+                pageSize: edges.length,
+                items: edges
+            };
+    } catch (e) {
+        console.log(e);
+    } finally {
+        // Ensures that the client will close when you finish/error
+        if (mongodb) await mongodb.close();
+    }
+
+}
+
 module.exports = {
-    createItems
+    createItems,
+    getBatchItem,
+    list
 };
